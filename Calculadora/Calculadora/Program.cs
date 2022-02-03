37fc4f33502bdcc5a52316fe60cd8e7a733e9d96
Laura Oliveira <lauranunest@hotmail.com>
2022-02-03 17:57:51 -0300

    first commit

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using LibCalc;
+
+namespace Calculadora
+{
+    class Program
+    {
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("============================ Calculadora ============================");
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            double a = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite a operação ( / | + | * | - ): ");
+            string operador = Console.ReadLine();
+
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("\nResultado: ");
+
+            switch (operador)
+            {
+                case "+":
+                    Funcoes.setSoma(a, b);
+                    Console.Write(Funcoes.getSoma());
+                    break;
+                case "-":
+                    Funcoes.setSub(a, b);
+                    Console.Write(Funcoes.getSub()); ;
+                    break;
+                case "*":
+                    Funcoes.setMult(a, b);
+                    Console.Write(Funcoes.getMult());
+                    break;
+                case "/":
+                    Funcoes.setDiv(a, b);
+                    Console.Write(Funcoes.getDiv());
+                    break;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ LibCalc/LibCalc/Funcoes.cs
@@ -0,0 +1,21 @@
+﻿namespace LibCalc
+{
+    public class Funcoes
+    {
+        private static double calculo;
+
+        public static void setSoma(double x,double y) { calculo = (x + y); }
+        public static double getSoma() { return calculo; }
+
+        public static void setSub(double x, double y) { calculo = (x - y); }
+        public static double getSub() { return calculo; }
+
+        public static void setDiv(double x, double y) { calculo = (x / y); }
+        public static double getDiv() { return calculo; }
+
+        public static void setMult(double x, double y) { calculo = (x * y); }
+        public static double getMult() { return calculo; }
+
+
+    }
+}
